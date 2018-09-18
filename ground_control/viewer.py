cef976c0a295867f16edee43ef3e4de355877339
ori ganoni <ori.ganoni@pg.canterbury.ac.nz>
2018-09-18 15:09:12 +1200

    more cleanning

--- ground_control/viewer.py
+++ ground_control/viewer.py
@@ -11,6 +11,7 @@ import argparse
 import numpy as np
 import config
 from gst import init_gst_reader,get_imgs
+from annotations import draw_txt
 import utils
 
 parser = argparse.ArgumentParser()
@@ -21,24 +22,6 @@ args = parser.parse_args()
 zmq_sub_v3d=utils.subscribe([config.topic_comp_vis],config.zmq_pub_comp_vis)
 zmq_sub_main=utils.subscribe([config.topic_main_telem],config.zmq_pub_main)
 
-def draw_txt(img,vd,md):
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    #print('-2-',md)
-    if 'ts' in md and 'range_avg' in vd:
-        line1='{:4.1f}s R{:3.2f}m'.format(md['ts'],vd['range_avg'])
-        cv2.putText(img,line1,(10,50), font, 0.5,(0,0,255),1,cv2.LINE_AA)
-
-    if 'lock' in md and md['lock']: 
-        line2='{:>4}bf {:4.2f}LR'.format(md['fb_cmd'],md['lock_range'])
-        if 'ud_cmd' in md:
-            line2+=' {:>4}ud {:>4}lr'.format(md['ud_cmd'],md['lr_cmd'])
-        cv2.putText(img,line2,(10,100), font, 0.5,(0,0,255),1,cv2.LINE_AA)
-    if vd.get('record_state',False):
-        cv2.putText(img,'REC '+vd['disk_usage'],(10,200),font, 0.5,(0,0,255),1,cv2.LINE_AA)
-    #cv2.putText(img,data_lines['last_cmd_str'][1],(10,250), font, 0.4,(0,0,255),1,cv2.LINE_AA)
-
-
-    
 if __name__=='__main__':
     init_gst_reader(2)
     sx,sy=config.pixelwidthx,config.pixelwidthy

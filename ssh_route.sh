9cc7b2f5c460cc8e909a1d2fc7cfc2cb4b0af470
uav <oga13@uclive.ac.nz>
2018-09-21 17:09:05 +1200

    testing without serial

--- ssh_route.sh
+++ ssh_route.sh
@@ -12,7 +12,12 @@ for i in $RP;do
   REMOTES="$REMOTES -R $i:127.0.0.1:$i"
 done
 
+#STEREO_IP=192.168.3.17
+#RASPPI_IP=192.168.2.2
+STEREO_IP=10.0.0.1
+RASPPI_IP=raspberrypi
+
 #echo $LOCALS
-CMD="ssh -t $LOCALS $REMOTES -L 2222:localhost:2222 pi@192.168.2.2 ssh -N -L 2222:localhost:22 $LOCALS $REMOTES stereo@192.168.3.17"
+CMD="ssh -t $LOCALS $REMOTES -L 2222:localhost:2222 pi@$RASPPI_IP ssh -N -L 2222:localhost:22 $LOCALS $REMOTES stereo@$STEREO_IP"
 echo $CMD
 $CMD

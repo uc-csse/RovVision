b5695d3bcd8f21d9bb6a701258e58c4e3e6f6af2
ori ganoni <ori.ganoni@pg.canterbury.ac.nz>
2018-10-04 16:59:55 +1300

    adding documentation and small modifications

--- arduino/uploadesp32.sh
+++ arduino/uploadesp32.sh
@@ -2,4 +2,4 @@
 #ARD_PATH=../../arduino-1.8.5/arduino
 ARD_PATH=../../arduino/arduino-1.8.5/arduino
 
-$ARD_PATH --upload --board esp32:esp32:esp32 --port /dev/ttyUSB0 GT-86.ino
+$ARD_PATH --upload --board esp32:esp32:esp32 --port /dev/ttyUSB0 GY-86.ino

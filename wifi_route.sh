058f932f3af59110594dafdc8a5dd15ead9f8d6c
uav <oga13@uclive.ac.nz>
2018-10-23 19:58:54 +1300

    some updates

---
+++ wifi_route.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+LOCALS=""
+REMOTES=""
+
+LP="6760 6761 8877 9921 5900 5901"
+RP="9117"
+
+for i in $LP;do 
+  LOCALS="$LOCALS -L $i:127.0.0.1:$i"
+done
+for i in $RP;do 
+  REMOTES="$REMOTES -R $i:127.0.0.1:$i"
+done
+
+#STEREO_IP=192.168.3.17
+#RASPPI_IP=192.168.2.2
+#STEREO_IP=10.0.0.1
+#RASPPI_IP=raspberrypi
+
+#MIDDLE_ADDR=oga13@linux.cosc.canterbury.ac.nz
+#STATION_ADDR=oga13@cs17062bq
+
+MIDDLE_ADDR=pi@192.168.2.2
+STATION_ADDR=stereo@192.168.3.17
+
+#STEREO_IP=10.0.0.1
+#echo $LOCALS
+CMD="ssh -t $LOCALS $REMOTES -L 2222:localhost:2222 $MIDDLE_ADDR ssh -N -L 2222:localhost:22 $LOCALS $REMOTES $STATION_ADDR"
+echo $CMD
+$CMD

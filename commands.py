b24a6d9005221f0960b64c99f0fe209623b6a729
ori ganoni <ori.ganoni@pg.canterbury.ac.nz>
2018-11-19 18:56:37 +1300

    adding commands

---
+++ commands.py
@@ -0,0 +1,19 @@
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+import config
+import controller
+import zmq
+
+import utils
+
+
+##### to use:
+## ipython
+## from commands import send
+## send(b'yaw_pid.D+=0.03;print(yaw_pid)')
+
+socket_pub = utils.publisher(config.zmq_pub_command)
+
+def send(cmd):
+    socket_pub.send_multipart([config.topic_command,cmd])
+
+
